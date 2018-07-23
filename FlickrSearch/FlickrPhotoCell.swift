95a3c3fdd170e25fb42b64c6dbd775f2c3923e1d
Denis Markov <keynsianets@gmail.com>
2018-07-23 17:05:33 +0300

    Add .gitignore file

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Denis Markov on 7/23/18.
+//  Copyright © 2018 Denis Markov. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+}
